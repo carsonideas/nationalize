08d010c86ed3c663e25bc13e824ac4e951c2e01e
Ideahub <douglascarson1121@gmail.com>
2025-05-27 17:58:20 +0300

    add the base functioning. i still have callign issue from the api not responding to other names only it is showing the catch

---
+++ index.js
@@ -0,0 +1,49 @@
+// const myNameInput = document.getElementById("form_search");
+const  myNameInput = document.querySelector ('input');
+const submitBtn = document.querySelector ('button');
+const result = document.querySelector (".result");
+const myBackResponse = document.querySelector (".result");
+
+submitBtn.addEventListener ("click", async (e) =>
+{
+    result.innerHTML = `Please wait, Houston will get back to you.....`
+    submitBtn.setAttribute ("disabled", true)
+try {
+e. preventDefault()
+const response = await fetch(`https://api.nationalize.io/?name=${myQuery}`)
+const result = await response.json();
+
+const {name, country_id, probability} = result;
+
+result.classList.add("result");
+
+    myBackResponse.innerHTML = `${result.name} is from ${result.country_id} with ${result.probability*100} certainty`
+    submitBtn.removeAttribute ("disabled")
+
+} 
+catch (e) 
+{
+    
+    myBackResponse.innerHTML = "Houston, we have a problem!!! Something went wrong please try again!!!"
+    
+    submitBtn.removeAttribute("disabled")
+    submitBtn. innerHTML =`<i class="fa-regular fa-magnifying-glass"></i>`
+} finally {}
+
+});
+
+
+
+
+
+
+
+// e. preventDefault()
+// const myQuery = myNameInput.value;
+// if (myQuery === "")
+// {
+//     myBackResponse.innerHTML = "Please enter a name!"
+//     return
+// }
+
+
